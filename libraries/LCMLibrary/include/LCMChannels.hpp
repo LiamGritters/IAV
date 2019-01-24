994e0560d9195ac018d8f036ef13c88e025f8e7b
LiamGritters <liam.gritters@gmail.com>
2019-01-24 17:03:17 -0500

    IAV Libraries -LCM Library includes all the variables for using the LCM Network -Utility Functions offers common functions that will be used in the IAV system

---
+++ libraries/LCMLibrary/include/LCMChannels.hpp
@@ -0,0 +1,26 @@
+/*
+ * LCMChannels.hpp
+ *
+ *  Created on: 2019-01-17
+ *      Author: liam
+ */
+
+#ifndef LIBRARIES_LCMLIBRARY_INCLUDE_LCMCHANNELS_HPP_
+#define LIBRARIES_LCMLIBRARY_INCLUDE_LCMCHANNELS_HPP_
+
+/****************************************
+ * INCLUDES
+ ****************************************/
+
+#include <string>
+
+/****************************************
+ * CONSTANTS
+ ****************************************/
+
+const std::string LidarChannel = "LIDAR";
+const std::string ControllerChannel = "CONTROLLER";
+const std::string IMUChannel = "IMU";
+const std::string GPSChannel = "GPS";
+
+#endif /* LIBRARIES_LCMLIBRARY_INCLUDE_LCMCHANNELS_HPP_ */
